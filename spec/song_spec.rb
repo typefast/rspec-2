1986e66719bd083ab6ab6830cce2308973ffc73e
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-28 15:38:21 +0000

    Add to_s test Song class

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -15,4 +15,10 @@ RSpec.describe Song do
       expect { Song.new("Title") }.to raise_error(ArgumentError)
     end
   end
+  
+  describe "#to_s" do 
+    it "returns a string representation of a song instance" do 
+      expect(@song.to_s).to eq("Title: #{@song.title} - Artist: #{@song.artist}")
+    end
+  end
 end
