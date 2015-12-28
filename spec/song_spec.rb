50ea4c2dc0448a245f7b13012ea047513ff14651
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-28 15:34:21 +0000

    Add argumentError test

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -3,12 +3,16 @@ require 'song'
 RSpec.describe Song do 
   
   before :each do 
-    @song = Song.new
+    @song = Song.new("Title", "Artist")
   end
   
   describe "#new" do 
     it "creates an instance of Song" do 
       expect(@song).to(be_an_instance_of(Song))
+    end
+    
+    it "With too few arguments throws an error" do 
+      expect { Song.new("Title") }.to raise_error(ArgumentError)
     end
   end
 end
