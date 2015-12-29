5a6676be920eddffacb47d89ef235e4b328cd96c
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-29 15:14:20 +0000

    Add empty hash initialize

--- spec/mp3_spec.rb
+++ spec/mp3_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Mp3 do
     it "creates an instance of Mp3" do 
       expect(@mp3).to(be_an_instance_of(Mp3))
     end
+    
+    it "Initializes Mp3 with an empty hash" do 
+      expect(@mp3.tracks).to eq({})
+    end
   end
   
 end
